198df41547713627e7e8c13c5e78fe7deb133099
ahongfenga <1004439185@qq.com>
2018-05-07 23:45:27 +0800

    test

--- src/main/java/com/xj/frame/FrameApplication.java
+++ src/main/java/com/xj/frame/FrameApplication.java
@@ -3,7 +3,7 @@ package com.xj.frame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class FrameApplication {
 
 	public static void main(String[] args) {
